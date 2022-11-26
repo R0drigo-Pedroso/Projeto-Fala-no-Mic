213b6067ccc1569518fc47691593515918f2b238
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-11-25 22:28:30 -0300

    trabalhando na page Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,70 @@
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ImageBackground,
+} from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import imageteste from "../../assets/image/festahiphop.jpg";
 
 function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <SafeAreaView style={estilos.container}>
       <StatusBar barStyle="default" />
-      <Text>Home! teste</Text>
-    </View>
+
+      <View style={estilos.spaceTitulo}>
+        <Text style={estilos.titulo}>Eventos</Text>
+      </View>
+
+      <View style={estilos.block}>
+        <View style={estilos.img}>
+          <ImageBackground style={estilos.tamanho} source={imageteste} />
+        </View>
+
+        <View style={estilos.descricao}>
+          <Text>teste</Text>
+          <Button
+            title="Press me"
+            onPress={() => Alert.alert("Simple Button pressed")}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+
+  spaceTitulo: {
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "white",
+    color: "black",
+  },
+
+  tamanho: {
+    width: 150,
+    height: 150,
+  },
+
+  block: {
+    backgroundColor: "blue",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    margin: 8,
+  },
+
+  img: {
+    width: "10%",
+  },
+});
