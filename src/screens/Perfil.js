e84698e9b733798a4247b4c7ad170bc12fae2513
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-12-06 11:26:18 -0300

    Codificação da tela de perfil
    Construindo overlay e campo que será exibido os dados do usuário

--- src/screens/Perfil.js
+++ src/screens/Perfil.js
@@ -5,10 +5,13 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
+  ScrollView,
 } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import fruta from "../../assets/image/fruta.jpg";
+import astronauta from "../../assets/image/astronauta.jpg";
 
 function Perfil() {
   const navigation = useNavigation();
@@ -16,24 +19,76 @@ function Perfil() {
   return (
     <SafeAreaView style={estilos.viewSafe}>
       <StatusBar barStyle="default" />
-      <View style={estilos.container}>
-        <ImageBackground
-          source={fruta}
-          resizeMode="cover"
-          style={estilos.imagem}
-        >
-          <Text style={estilos.text}>Inside</Text>
-        </ImageBackground>
-      </View>
+
+      <ScrollView>
+        <View style={estilos.container}>
+          <ImageBackground
+            source={fruta}
+            resizeMode="cover"
+            style={estilos.imagem}
+          >
+            <View style={estilos.viewFoto}>
+              <Image source={astronauta} style={estilos.foto} />
+              <Text style={estilos.usuario}>Nome do Usuário</Text>
+              <Text style={estilos.endereco}>São Paulo, Brasil</Text>
+            </View>
+          </ImageBackground>
+
+          <View style={estilos.backgroundCard}>
+            <View style={estilos.card}>
+              <Text style={estilos.texto}>Descrição</Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
 export default Perfil;
 
 const estilos = StyleSheet.create({
-  viewSafe: { flex: 1 },
+  viewSafe: { flex: 1, backgroundColor: "#F7F7F7" },
   container: {},
   imagem: {
+    height: "90%",
+  },
+  user: {
+    height: "50%",
+    borderRadius: "50%",
+  },
+  foto: {
+    width: 200,
+    height: 200,
+    borderRadius: 350,
+  },
+  viewFoto: {
+    height: "90%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0, 0.8)",
+  },
+  usuario: {
+    color: "white",
+    fontSize: 18,
+    marginVertical: 10,
+    fontWeight: "bold",
+  },
+  endereco: {
+    color: "white",
+    fontSize: 16,
+    marginVertical: 5,
+  },
+  card: {
+    backgroundColor: "#FFFFFF",
     height: "80%",
+    padding: 8,
+    width: "90%",
+    justifyContent: "center",
+  },
+  texto: {
+    color: "black",
+  },
+  backgroundCard: {
+    alignItems: "center",
   },
 });
