48553bd5031ba69b0e016e8961ba8176bcbf502f
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-12-07 11:41:00 -0300

    Formatação da página Cadastro

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,67 +1,129 @@
 import {
   StatusBar,
+  ScrollView,
   StyleSheet,
   Text,
   View,
   Image,
   Pressable,
+  TextInput,
+  Button,
 } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import imageteste from "../../assets/image/festahiphop.jpg";
+import { useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import logo from "../../assets/image/logo_fala_no_mic.png";
 
-function Home() {
+function Cadastro() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   return (
     <SafeAreaView>
       <StatusBar barStyle="default" />
-      <Text style={estilos.titulo}>Eventos</Text>
-
-      <View style={estilos.areaConteudo}>
-        <View style={estilos.image}>
-          <Image style={estilos.imageTamanho} source={imageteste} />
-        </View>
-
-        <View style={estilos.descricao}>
-          <View>
-            <Text>Titulo</Text>
-            <Text>descrição</Text>
+      <ScrollView>
+        <View style={estilos.container}>
+          <View style={estilos.containerFoto}>
+            <Image source={logo} style={estilos.foto} />
           </View>
-          <Pressable>
-            <Text>Saiba +</Text>
-          </Pressable>
+
+          <View style={estilos.containerTitulo}>
+            <Text style={estilos.titulo}>Cadastrar</Text>
+          </View>
+
+          <View style={estilos.inputs}>
+            <TextInput
+              style={estilos.campoNome}
+              onChangeText={setNome}
+              placeholder="nome"
+              value={nome}
+            />
+
+            <TextInput
+              style={estilos.campoEmail}
+              onChangeText={setEmail}
+              placeholder="exemplo@exemplo.com"
+              value={email}
+            />
+
+            <TextInput
+              style={estilos.campoSenha}
+              onChangeText={setSenha}
+              placeholder="senha"
+              value={senha}
+            />
+          </View>
+          <Pressable style={estilos.botao} />
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
-export default Home;
+export default Cadastro;
 
 const estilos = StyleSheet.create({
   titulo: {
+    fontSize: 22,
+  },
+  campoNome: {
+    fontSize: 16,
+    height: 54,
+    margin: 15,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "white",
+    borderRadius: 10,
+    width: "85%",
+    // shadowColor: "#171717",
+    // shadowOffset: { width: -2, height: 4 },
+    // shadowOpacity: 0.2,
+    // shadowRadius: 3,
+  },
+  campoEmail: {
+    fontSize: 16,
+    height: 54,
+    margin: 20,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "white",
+    borderRadius: 10,
+    width: "85%",
+  },
+  campoSenha: {
+    fontSize: 16,
+    height: 54,
+    margin: 20,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "white",
+    borderRadius: 10,
+    width: "85%",
+  },
+  campoConfirmarSenha: {
+    fontSize: 16,
+    height: 64,
+    margin: 20,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "white",
+    borderRadius: 10,
+  },
+  foto: {
     textAlign: "center",
-    fontSize: 24,
-    padding: 8,
   },
-
-  areaConteudo: {
-    width: "100%",
-    flexDirection: "row",
-    padding: 8,
+  containerFoto: {
+    alignItems: "center",
   },
-
-  image: {
-    flex: 0.3,
+  containerTitulo: {
+    paddingHorizontal: 25,
   },
-
-  imageTamanho: {
-    width: "100%",
-    height: 150,
+  inputs: {
+    alignItems: "center",
   },
-
-  descricao: {
-    flex: 0.7,
-    backgroundColor: "blue",
-    padding: 8,
+  botao: {
+    backgroundColor: "red",
   },
 });
