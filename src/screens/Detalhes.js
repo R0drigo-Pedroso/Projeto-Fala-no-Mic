10f39d684fa1ffba33a76f469b3cf3f33a5fbc93
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-02-11 23:54:31 -0300

    Update Detalhes.js

--- src/screens/Detalhes.js
+++ src/screens/Detalhes.js
@@ -47,14 +47,13 @@ const Detalhes = () => {
 
             <View style={estilos.descricao}>
               <Text style={estilos.tituloDescricao}>Descrição:</Text>
-              <Text style={estilos.descricaoEvento}>id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas sed tempus urna et pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna eget est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus et netus et malesuada. </Text>
+              <Text style={estilos.descricaoEvento}>id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas sed tempus urna et pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna eget est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus et netus et malesuada.</Text>
             </View>
 
             <View style={estilos.local}>
               <Text style={estilos.tituloLocalizacao}>Localização:</Text>
               <Text style={estilos.endereco}>AV. Lorem Ipsum is simply dummy, 200 - Bairro Cidade Líder </Text>
             </View>
-
           </View>
         </FontLoader>
       </ScrollView>
